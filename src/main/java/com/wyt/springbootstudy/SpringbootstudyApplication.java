c7986e4eef0eb5dd3b7a9eb51d06f56300aabbad
nimentaogege1992 <1334727150@qq.com>
2019-10-05 17:24:00 +0800

    first commit

---
+++ src/main/java/com/wyt/springbootstudy/SpringbootstudyApplication.java
@@ -0,0 +1,13 @@
+package com.wyt.springbootstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootstudyApplication.class, args);
+	}
+
+}
